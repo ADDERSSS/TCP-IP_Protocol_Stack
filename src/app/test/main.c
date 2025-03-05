86562fe9795945b5c6292e9a2be05fc85eb60374
ADDERS <312732105@qq.com>
2025-03-05 22:21:33 +0800

    pcap_send_packet

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -1,22 +1,35 @@
-﻿/**
- * @file main.c
- * @author lishutong (527676163@qq.com)
- * @brief 测试主程序，完成一些简单的测试主程序
- * @version 0.1
- * @date 2022-10-23
- *
- * @copyright Copyright (c) 2022
- * @note 该源码配套相应的视频课程，请见源码仓库下面的README.md
- */
-#include <stdio.h>
+﻿#include <stdio.h>
 #include "sys_plat.h"
 
 int main (void) {
-	// 以下是测试代码，可以删掉
-	// 打开物理网卡，设置好硬件地址
-	static const uint8_t netdev0_hwaddr[] = { 0x00, 0x50, 0x56, 0xc0, 0x00, 0x11 };
-	pcap_t* pcap = pcap_device_open("192.168.74.1", netdev0_hwaddr);
-	sys_sleep(1000);
+	//static const uint8_t netdev0_hwaddr[] = { 0x00, 0x50, 0x56, 0xc0, 0x00, 0x11 };
+	pcap_t* pcap = pcap_device_open(netdev0_phy_ip, netdev0_hwaddr);
+	while (pcap) {
+		static uint8_t buffer[1024];
+		static int counter; 
+		struct pcap_pkthdr * pkthdr;
+		const uint8_t * pkt_data;
+
+		plat_printf("begin test : %d\n", counter++);
+		for (int i = 0; i < sizeof(buffer); i ++) {
+			buffer[i] = i;
+		}
+
+		if (pcap_next_ex(pcap, &pkthdr, &pkt_data) != 1) {
+			continue;
+		}
+
+		int len = pkthdr->len > sizeof(buffer) ? sizeof(buffer) : pkthdr->len;
+		plat_memcpy(buffer, pkt_data, len);
+
+		buffer[0] = 1;
+		buffer[1] = 2;
+
+		if (pcap_inject(pcap, buffer, sizeof(buffer)) == -1) {
+			plat_printf("pcap send: send packet failed %s \n", pcap_geterr(pcap));
+			break;
+		}
+	}
 
 	printf("Hello, world");
 	return 0;
