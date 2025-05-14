a000b92db9de1a5e1213158cfc31311015aa6cf6
ADDERS <312732105@qq.com>
2025-05-14 17:18:28 +0800

    file_create&thread_create

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -2,6 +2,8 @@
 #include "sys_plat.h"
 #include "echo/tcp_echo_client.h"
 #include "echo/tcp_echo_server.h"
+#include "net.h"
+#include "netif_pcap.h"
 
 static sys_mutex_t mutex;
 static sys_sem_t sem;
@@ -64,49 +66,23 @@ void thread2_entry (void * arg) {
 	}
 }
 
+net_err_t netdev_init (void) {
+	netif_pcap_open();
+
+	return NET_ERR_OK;
+}
+
+
 int main (void) {
+	net_init();
 
-	sem = sys_sem_create(0);  
-	read_sem = sys_sem_create(0);
-	write_sem = sys_sem_create(sizeof(buffer_g));
-	mutex = sys_mutex_create();
-	sz_mutex = sys_mutex_create();
+	net_start();
 
-	// sys_thread_create(thread1_entry, "AAAA");
-	// sys_thread_create(thread2_entry, "BBBB");
+	netdev_init();
 
-	//tcp_echo_client_start(friend0_ip, 5000);
-	tcp_echo_server_start(5000);
-
-	//static const uint8_t netdev0_hwaddr[] = { 0x00, 0x50, 0x56, 0xc0, 0x00, 0x11 };
-	pcap_t* pcap = pcap_device_open(netdev0_phy_ip, netdev0_hwaddr);
-	while (pcap) {
-		static uint8_t buffer[1024];
-		static int counter; 
-		struct pcap_pkthdr * pkthdr;
-		const uint8_t * pkt_data;
-
-		plat_printf("begin test : %d\n", counter++);
-		for (int i = 0; i < sizeof(buffer); i ++) {
-			buffer[i] = i;
-		}
-
-		if (pcap_next_ex(pcap, &pkthdr, &pkt_data) != 1) {
-			continue;
-		}
-
-		int len = pkthdr->len > sizeof(buffer) ? sizeof(buffer) : pkthdr->len;
-		plat_memcpy(buffer, pkt_data, len);
-
-		buffer[0] = 1;
-		buffer[1] = 2;
-
-		if (pcap_inject(pcap, buffer, sizeof(buffer)) == -1) {
-			plat_printf("pcap send: send packet failed %s \n", pcap_geterr(pcap));
-			break;
-		}
+	while (1) {
+		sys_sleep(10);
 	}
 
-	printf("Hello, world");
 	return 0;
 } 
